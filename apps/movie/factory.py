9d20e96611847d8bc223888ebf3ba07a11de56ef
aravindacreoit <aravinda@creoit.com>
2019-07-09 19:14:10 +0530

    Temp commit

---
+++ apps/movie/factory.py
@@ -0,0 +1,18 @@
+import factory
+
+from faker import Faker
+
+from apps.user import models
+
+fake = Faker()
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+    email = factory.LazyAttribute(
+        lambda obj: f'{obj.first_name.lower()}_{obj.last_name.lower()}@example.com')
+    username = factory.LazyAttribute(lambda obj: obj.email)
+
+    class Meta:
+        model = models.User
