9a15328e155a1d6dfc90d89f62e98ea947c6297c
aravindas4 <30445925+aravindas4@users.noreply.github.com>
2019-07-14 15:28:19 +0530

    Feature/movie apis (#2)
    Add env sample
    Add silk package
    Add person APIs
    Add person List and Detail APIs
    Add query optimization

--- apps/apis/v1/movie/serializers.py
+++ apps/apis/v1/movie/serializers.py
@@ -10,3 +10,43 @@ class MovieModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Movie
         fields = ('id', 'name', 'plot', 'rating')
+
+
+class MoviePersonModelSerializer(serializers.ModelSerializer):
+    movie_name = serializers.CharField(source='movie.name')
+    movie_id = serializers.CharField(source='movie.id')
+    role = ChoicesField(choices=models.MoviePerson.ROLE)
+
+    class Meta:
+        model = models.MoviePerson
+        fields = ('movie_name', 'role', 'movie_id')
+
+
+class PersonListSerialiazer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Person
+        fields = ('id', 'name')
+
+
+class PersonDetailSerializer(serializers.ModelSerializer):
+    actor = serializers.SerializerMethodField()
+    director = serializers.SerializerMethodField()
+    writer = serializers.SerializerMethodField()
+
+    class Meta:
+        model = models.Person
+        fields = ('id', 'name', 'date_of_birth', 'died_on', 'actor',
+                  'director', 'writer')
+
+    def get_actor(self, obj):
+        return MoviePersonModelSerializer(obj.movieperson_set.actors(),
+                                          many=True).data
+
+    def get_director(self, obj):
+        return MoviePersonModelSerializer(obj.movieperson_set.directors(),
+                                          many=True).data
+
+    def get_writer(self, obj):
+        return MoviePersonModelSerializer(obj.movieperson_set.writers(),
+                                          many=True).data
--- apps/apis/v1/movie/urls.py
+++ apps/apis/v1/movie/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 
 from rest_framework.routers import SimpleRouter
 
-from .api import MovieModelViewSet
+from .api import MovieModelViewSet, PersonModelViewSet
 
 router = SimpleRouter()
+router.register('person', PersonModelViewSet, basename='movie-person')
 router.register('', MovieModelViewSet, basename='movie')
 
 urlpatterns = router.urls
